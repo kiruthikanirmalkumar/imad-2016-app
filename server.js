9f8ef172206c5ba00ae10b763045d2ad31060e47
kiruthikanirmalkumar <kiruthikanirmal1@gmail.com>
2016-11-18 17:48:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,36 +1,35 @@
-function getUserProperties() {
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
 
-    // Replace the placeholder value with the target user's credentials.
-    var targetUser = "domainName\\userName";
+// Create a server
+http.createServer( function (request, response) {  
+   // Parse the request containing file name
+   var pathname = url.parse(request.url).pathname;
+   
+   // Print the name of the file for which request is made.
+   console.log("Request for " + pathname + " received.");
+   
+   // Read the requested file content from file system
+   fs.readFile(pathname.substr(1), function (err, data) {
+      if (err) {
+         console.log(err);
+         // HTTP Status: 404 : NOT FOUND
+         // Content Type: text/plain
+         response.writeHead(404, {'Content-Type': 'text/html'});
+      }else {	
+         //Page found	  
+         // HTTP Status: 200 : OK
+         // Content Type: text/plain
+         response.writeHead(200, {'Content-Type': 'text/html'});	
+         
+         // Write the content of the file to response body
+         response.write(data.toString());		
+      }
+      // Send the response body 
+      response.end();
+   });   
+}).listen(8080);
 
-    // Get the current client context and PeopleManager instance.
-    var clientContext = new SP.ClientContext.get_current();
-    var peopleManager = new SP.UserProfiles.PeopleManager(clientContext);
-
-    // Get user properties for the target user.
-    // To get the PersonProperties object for the current user, use the
-    // getMyProperties method.
-    personProperties = peopleManager.getPropertiesFor(targetUser);
-
-    // Load the PersonProperties object and send the request.
-    clientContext.load(personProperties);
-    clientContext.executeQueryAsync(onRequestSuccess, onRequestFail);
-}
-
-// This function runs if the executeQueryAsync call succeeds.
-function onRequestSuccess() {
-
-    // Get a property directly from the PersonProperties object.
-    var messageText = " \"DisplayName\" property is "
-        + personProperties.get_displayName();
-
-    // Get a property from the UserProfileProperties property.
-    messageText += "<br />\"Department\" property is "
-        + personProperties.get_userProfileProperties()['Department'];
-    $get("results").innerHTML = messageText;
-}
-
-// This function runs if the executeQueryAsync call fails.
-function onRequestFail(sender, args) {
-    $get("results").innerHTML = "Error: " + args.get_message();
-}
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8080/');
