d9a8cda8ace610bf2c7552287d2bdca1dd6c81aa
kiruthikanirmalkumar <kiruthikanirmal1@gmail.com>
2016-11-18 17:48:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,37 @@
 console.log('Loaded!');
+function getUserProperties() {
+
+    // Replace the placeholder value with the target user's credentials.
+    var targetUser = "domainName\\userName";
+
+    // Get the current client context and PeopleManager instance.
+    var clientContext = new SP.ClientContext.get_current();
+    var peopleManager = new SP.UserProfiles.PeopleManager(clientContext);
+
+    // Get user properties for the target user.
+    // To get the PersonProperties object for the current user, use the
+    // getMyProperties method.
+    personProperties = peopleManager.getPropertiesFor(targetUser);
+
+    // Load the PersonProperties object and send the request.
+    clientContext.load(personProperties);
+    clientContext.executeQueryAsync(onRequestSuccess, onRequestFail);
+}
+
+// This function runs if the executeQueryAsync call succeeds.
+function onRequestSuccess() {
+
+    // Get a property directly from the PersonProperties object.
+    var messageText = " \"DisplayName\" property is "
+        + personProperties.get_displayName();
+
+    // Get a property from the UserProfileProperties property.
+    messageText += "<br />\"Department\" property is "
+        + personProperties.get_userProfileProperties()['Department'];
+    $get("results").innerHTML = messageText;
+}
+
+// This function runs if the executeQueryAsync call fails.
+function onRequestFail(sender, args) {
+    $get("results").innerHTML = "Error: " + args.get_message();
+}
